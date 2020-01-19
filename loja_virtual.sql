a0544c6bbf3841c508c917efee994b1d85febeab
Filipe Moreira <filimor@posteo.com.br>
2020-01-19 04:52:03 -0300

    Relacionamentos Um para Um e Um para Muitos

--- loja_virtual.sql
+++ loja_virtual.sql
@@ -1,7 +1,36 @@
 CREATE DATABASE db_loja_virtual;
-USE db_loja_virtual;
+
+-- USE db_loja_virtual;
+
 CREATE TABLE tb_produtos(
-    id_produto INT NOT NULL PRIMARY KEY,
+    id_produto INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     produto VARCHAR(200) NOT NULL,
     valor FLOAT(8,2) NOT NULL
 );
+
+CREATE TABLE tb_descricoes_tecnicas(
+    id_descricao_tecnica INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_produto INT NOT NULL,
+    descricao_tecnica TEXT NOT NULL,
+    FOREIGN KEY(id_produto) REFERENCES tb_produtos(id_produto)
+);
+
+INSERT INTO tb_produtos(produto, valor) VALUES ('Notebook Dell Inspiron Ultrafino Intel Core i7, 16GB RAM e 240GB SSD', 3500.00);
+INSERT INTO tb_produtos(produto, valor) VALUES ('Smart TV LED 40" Samsung Full HD 2 HDMI 1 USB Wi-Fi Integrado', 1475.54);
+INSERT INTO tb_produtos(produto, valor) VALUES ('Smartphone LG K10 Dual Chip Android 7.0 4G Wi-Fi Câmera de 13MP', 629.99);
+
+INSERT INTO tb_descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (1, 'O novo Inspiron Dell oferece um design elegante e tela infinita que amplia seus sentidos, mantendo a sofisticação e medidas compactas...');
+INSERT INTO tb_descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (2, 'A smart TV da Samsung possui tela de 40" e oferece resolução Full HD, imagens duas vezes melhores que TVs HDs padrão...');
+INSERT INTO tb_descricoes_tecnicas(id_produto, descricao_tecnica) VALUES (3, 'Saia da mesmice. O smartphone LG está mais divertido, rápido, fácil, cheio de selfies e com tela HD de incríveis 5,3"...');
+
+CREATE TABLE tb_imagens(
+    id_imagem INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_produto INT NOT NULL,
+    FOREIGN KEY(id_produto) REFERENCES tb_produtos(id_produto),
+    url_imagem VARCHAR(200) NOT NULL
+);
+
+INSERT INTO tb_imagens(id_produto, url_imagem) VALUES (1, 'notebook_1.jpg'), (1, 'notebook_2.jpg'), (1, 'notebook_3.jpg');
+INSERT INTO tb_imagens(id_produto, url_imagem) VALUES (2, 'smarttv_1.jpg'), (1, 'smarttv_2.jpg');
+INSERT INTO tb_imagens(id_produto, url_imagem) VALUES (3, 'smartphone_1.jpg');
+
