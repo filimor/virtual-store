955c4f8c97fed3637890c7c49bacac18ced72280
Filipe Moreira <filimor@posteo.com.br>
2020-01-20 19:56:07 -0300

    Alias

--- joins.sql
+++ joins.sql
@@ -25,3 +25,12 @@ INNER JOIN tb_pedidos_produtos ON (tb_pedidos.id_pedido = tb_pedidos_produtos.id
 SELECT * FROM tb_pedidos
 INNER JOIN tb_pedidos_produtos ON (tb_pedidos.id_pedido = tb_pedidos_produtos.id_pedido)
 INNER JOIN tb_produtos ON (tb_pedidos_produtos.id_produto = tb_produtos.id_produto);
+
+-- Usando Alias
+
+SELECT * FROM tb_produtos LEFT JOIN tb_descricoes_tecnicas ON (tb_produtos.id_produto = tb_descricoes_tecnicas.id_produto);
+
+SELECT p.id_produto, p.produto, p.valor, dt.descicao_tecnica 
+FROM tb_produtos AS p LEFT JOIN tb_descricoes_tecnicas AS dt ON (p.id_produto = dt.id_produto)
+WHERE p.valor >= 500
+ORDER BY p.valor ASC;
